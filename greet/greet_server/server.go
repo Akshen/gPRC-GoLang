bb6adcf85c595e248805a73e86d4cb306398099a
Akshen  Doke <akshen@Akshens-MacBook-Air.local>
2021-03-14 16:37:41 +0000

    Server Code
    proto file working, server.go working

--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -1,27 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 
-	"github.com/Akshen/gRPC-GoLang/greet/greetpb"
+	"github.com/gRPC-GoLang/greet/greetpb"
 	"google.golang.org/grpc"
 )
 
 type server struct{}
 
 func main() {
-	//fmt.Println("Hello, World!")
+	fmt.Println("Hello, World")
 
 	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
+		log.Fatalf("failed to listen %v", err)
 	}
 
 	s := grpc.NewServer()
 	greetpb.RegisterGreetServiceServer(s, &server{})
 
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v ", err)
+		log.Fatalf("Failed to server: %v", err)
 	}
 }
