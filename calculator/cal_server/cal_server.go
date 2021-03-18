36351f6e8059e2299d497284548370397d0eefb2
Akshen  Doke <doke.akshen@gmail.com>
2021-03-18 15:49:09 +0000

    reflection added
    Inorder to interact with reflection, you need to install evans cli

--- calculator/cal_server/cal_server.go
+++ calculator/cal_server/cal_server.go
@@ -12,6 +12,7 @@ import (
 	"github.com/gRPC-GoLang/calculator/calpb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc/status"
 )
 
@@ -119,6 +120,7 @@ func main() {
 
 	s := grpc.NewServer()
 	calpb.RegisterCalServiceServer(s, &server{})
+	reflection.Register(s)
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
